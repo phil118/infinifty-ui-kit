c97ddc33e6d0bf9c1dd9fdb4bc03a34c8bd9590e
Phil Jenkins <p.jenkins118@gmail.com>
2021-12-15 18:47:25 +0000

    started workings of icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,41 @@ module.exports = function(grunt) {
                     implementation: sass //and this part
                 },
                 files: {
-                    "css/infinifty.css": "scss/infinifty.scss"
+                    "dist/css/infinifty.css": "scss/infinifty.scss"
                 }
+            }
+        },
+        svg_sprite: {
+            options: {
+                shape: {
+                    id: { 
+                        generator: function(name) {  
+                            var n = name.toLowerCase().replace(/ /g,"-").replace("_","-").replace(".svg", "").replace(/-v1$/g, "");
+                            console.log(n);
+                            return n;
+                        }
+                    }                         
+                },
+                mode: {
+                    symbol: true,
+                    view: {           // Activate the «view» mode
+                        bust: false,
+                        render: {
+                          scss: true    // Activate Sass output (with default options)
+                        }
+                    },
+                    css: {        // Activate the «css» mode
+                        render: {
+                          css: true // Activate CSS output (with default options)
+                        }
+                    }
+                }  
+            },
+            default: {
+                expand: true,
+                cwd: 'images/icons/32px',
+                src: ['**/*.svg'],
+                dest: 'dist/icons'
             }
         },
 
@@ -31,5 +64,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-svg-sprite');
+    grunt.registerTask('sprites', ['svg_sprite']);
     grunt.registerTask('default', ['sass', 'watch']);
 }
